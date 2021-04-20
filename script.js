7331d4ec686811fa4f2f1a037d8090fe36501b44
realbaotrung <realbaotrung@gmail.com>
2021-04-20 23:17:04 +0700

    Edit script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+/**
+ * Print out each character of a word
+ */
+
+let string = "Hello World";
+
+let println = (str) => {
+  for (let chr of str) {
+    console.log(chr);
+  }
+};
+
+println(string);
